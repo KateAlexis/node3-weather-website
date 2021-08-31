e2375379302d5b8f11a94d8ea57672b3e8709a92
kate Alexis Santos <kate.alexis.g.santos@gmail.com>
2021-08-31 11:46:45 +0800

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,16 +12,15 @@ const forecast = (latitude, longitude, callback) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
     } else if (body.error) {
-      callback("Unable to connect to find location!", undefined);
+      callback("Unable to find location!", undefined);
     } else {
-      callback(
-        undefined,
+        callback(undefined,
         body.current.weather_descriptions[0] +
           ". It is currently " +
           body.current.temperature +
           " degrees out. It feels like " +
           body.current.feelslike +
-          " degrees out."
+          " degrees out. The humidity is " + body.current.humidity + "%."
       );
     }
   });
